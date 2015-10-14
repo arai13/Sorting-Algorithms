72a4051466d63a05eaa487891034b426393383f9
Ashutosh Rai <arai13@earlham.edu>
2015-10-14 15:51:29 -0400

    added dynamic array

--- main-program.cpp
+++ main-program.cpp
@@ -9,23 +9,45 @@ Contents:
 *************************************/
 #include <iostream>
 #include <string>
+#include <new>
 
 using namespace std;
 
 int main(){
   char start_prompt; 
-  int temp; //variable to add number to array
+  int array_size; //size of the array
+  double *array_ptr; //pointer for array
+  //int nullptr = NULL;
+
   cout << "Sorting Program" << endl;
   cout << "Do you want to start an array?(y/n): ";
   cin >> start_prompt;
   start_prompt = toupper(start_prompt);
   
-  while (start_prompt != 'Y' && start_prompt != 'N') {
-    cout << "Please enter a valid response (y/n): " << endl;
+  while (start_prompt != 'Y' && start_prompt != 'N') { //while loop for validation of start_prompt
+    cout << "Please enter a valid response (y/n): ";
     cin >> start_prompt;
-    start_prompt = toupper(start_prompt);
-  }    
+    start_prompt = toupper(start_prompt);}    
 
-  cout << start_prompt << endl;
+  if (start_prompt == 'N') {
+    cout << "Thank you for using this program. Have a great day!";
+    return 0;}
+  else {
+    cout << "How many numbers do you want to add to the array?" << endl;
+    cin >> array_size;
+    array_ptr = new (nothrow) double[array_size]; 
+    
+    if (array_ptr == NULL) {
+      cout << "Error: memory could not be allocated" << endl; }
+    else {
+      for (int n = 0; n < array_size; n++) {
+	cout << "Enter a number to add to the array: ";
+	cin >> array_ptr[n];}}
+
+    cout << "You have entered: ";
+    for (int n=0; n < array_size; n++) {
+      cout << array_ptr[n] << ", ";}
+    delete[] array_ptr;}
+  
   return 0;
 }
