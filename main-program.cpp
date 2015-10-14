9ffc7e4700e645cc70da0e6cdcd9cc0f67491eaf
Ashutosh Rai <arai13@earlham.edu>
2015-10-13 22:25:39 -0400

    start_prompt for driver program

--- main-program.cpp
+++ main-program.cpp
@@ -7,3 +7,25 @@ Authors: Ashutosh Rai and Niraj Parajuli
 Contents:
 
 *************************************/
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+  char start_prompt; 
+  int temp; //variable to add number to array
+  cout << "Sorting Program" << endl;
+  cout << "Do you want to start an array?(y/n): ";
+  cin >> start_prompt;
+  start_prompt = toupper(start_prompt);
+  
+  while (start_prompt != 'Y' && start_prompt != 'N') {
+    cout << "Please enter a valid response (y/n): " << endl;
+    cin >> start_prompt;
+    start_prompt = toupper(start_prompt);
+  }    
+
+  cout << start_prompt << endl;
+  return 0;
+}
